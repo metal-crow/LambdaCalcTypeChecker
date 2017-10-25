431b1298ef9b8662327d11b3486f61ced91d73ab
metal-crow <frankcrisanti@aol.com>
2017-10-25 15:03:24 -0400

    change way of doing it

--- src/main.scala
+++ src/main.scala
@@ -31,94 +31,111 @@ object main {
   
   }
   
-  def unifyConstraints(constraints: scala.collection.mutable.ListBuffer[Tuple2[Type,Type]]) : Option[Map[VarType,Type]] = {
+  def unifyConstraints(constraints_a: scala.collection.mutable.ListBuffer[Tuple2[Type,Type]]) : Option[Map[VarType,Type]] = {
+    var constraints = constraints_a;
     val subs = scala.collection.mutable.Map[VarType,Type]();
+    var still_propigations = true;
     
-    for(constraint <- constraints){
-      constraint match {
-        //case where constraint is tautology is ignored
-        case (_:IntType, _:IntType) => {};
-        case (_:BoolType, _:BoolType) => {};
-        
-        //case where constraint is contradiction is failure
-        case (_:BoolType, _:IntType) => return None;
-        case (_:IntType, _:BoolType) => return None;
-        case (_:ArrowType, _:IntType) => return None;
-        case (_:IntType, _:ArrowType) => return None;
-        case (_:ArrowType, _:BoolType) => return None;
-        case (_:BoolType, _:ArrowType) => return None;
-
-        //case of a var mapping is checked against current substitution list
-        case (variable:VarType, t:IntType) =>
-          if(subs.contains(variable) && !subs(variable).equals(t)){
-            return None;
-          } else{
-            subs += (variable -> t);
-          }
-        case (t:IntType, variable:VarType) =>
-          if(subs.contains(variable) && !subs(variable).equals(t)){
-            return None;
-          } else{
-            subs += (variable -> t);
-          }
-        case (variable:VarType, t:BoolType) =>
-          if(subs.contains(variable) && !subs(variable).equals(t)){
-            return None;
-          } else{
-            subs += (variable -> t);
-          }
-        case (t:BoolType, variable:VarType) =>
-          if(subs.contains(variable) && !subs(variable).equals(t)){
-            return None;
-          } else{
-            subs += (variable -> t);
-          }
-          
-        case (variable:VarType, arrow:ArrowType) => 
-          (arrow.src,arrow.dst) match {
-            //TODO
-            case (src:IntType, dst:IntType) => print("TODO");
-          }
-        case (arrow:ArrowType, variable:VarType) =>
-            //TODO
-          
-        //case of a var mapping to a var, will check that they are the same type after all substitutions
-        case (vara:VarType, varb:VarType) =>
-          if(subs.contains(vara) && !subs(vara).equals(varb)){
-            return None;
-          } else{
-            subs += (vara -> varb);
-          }
-          
-        //case of an arrow to arrow, generate two additional constraints
-        case (arrowa: ArrowType, arrowb:ArrowType) =>
-          constraints += new Tuple2(arrowa.src,arrowb.src);
-          constraints += new Tuple2(arrowa.dst,arrowb.dst);
-      }
-    }
-    
-    //check substitutions to verify that var to var mappings are propagated
-    
-    //while the mapping contains a var to var mapping
-    while(subs.exists(_._2.isInstanceOf[VarType]))
-    {
-      val subsitution: (VarType, VarType) = subs.find(_._2.isInstanceOf[VarType]).get.asInstanceOf[(VarType, VarType)];
-      //for var X
-      val sub_var = subsitution._1;
-      //1 get X's var mapping (Y).
-      val sub_mapping = subsitution._2;
-      //If Y is a free variable, cannot solve
-      if(!subs.contains(sub_mapping)){
-        return None;
-      }
-      //check if Y is solved (not of type var)
-      if(!subs(sub_mapping).getClass().equals(VarType)
+    while(still_propigations){
+      still_propigations = false;
       
-      //propagate is so, goto 1 if not with X=Y
-      //if X==X, circular dependency. None
+      for(constraint <- constraints){
+        constraint match {
+          //case where constraint is tautology is ignored
+          case (_:IntType, _:IntType) => {};
+          case (_:BoolType, _:BoolType) => {};
+          
+          //case where constraint is contradiction is failure
+          case (_:BoolType, _:IntType) => return None;
+          case (_:IntType, _:BoolType) => return None;
+          case (_:ArrowType, _:IntType) => return None;
+          case (_:IntType, _:ArrowType) => return None;
+          case (_:ArrowType, _:BoolType) => return None;
+          case (_:BoolType, _:ArrowType) => return None;
+  
+          //case of a var mapping is checked against current substitution list
+          case (variable:VarType, t:IntType) =>
+            if(subs.contains(variable) && !subs(variable).equals(t)){
+              return None;
+            } else{
+              //replace all instances of var in constraints with the type
+              constraints = replaceAllVarWithType(constraints, variable, t);
+              subs += (variable -> t);
+            }
+          case (t:IntType, variable:VarType) =>
+            if(subs.contains(variable) && !subs(variable).equals(t)){
+              return None;
+            } else{
+              constraints = replaceAllVarWithType(constraints, variable, t);
+              subs += (variable -> t);
+            }
+          case (variable:VarType, t:BoolType) =>
+            if(subs.contains(variable) && !subs(variable).equals(t)){
+              return None;
+            } else{
+              constraints = replaceAllVarWithType(constraints, variable, t);
+              subs += (variable -> t);
+            }
+          case (t:BoolType, variable:VarType) =>
+            if(subs.contains(variable) && !subs(variable).equals(t)){
+              return None;
+            } else{
+              constraints = replaceAllVarWithType(constraints, variable, t);
+              subs += (variable -> t);
+            }
+            
+          case (variable:VarType, arrow:ArrowType) => 
+            if(subs.contains(variable) && !subs(variable).equals(arrow)){
+              return None;
+            } else{
+              //TODO check if arrow type contains undefined vars. Need to continue propagation if so
+              constraints = replaceAllVarWithType(constraints, variable, arrow);
+              subs += (variable -> arrow);
+            }
+          case (arrow:ArrowType, variable:VarType) =>
+            if(subs.contains(variable) && !subs(variable).equals(arrow)){
+              return None;
+            } else{
+              constraints = replaceAllVarWithType(constraints, variable, arrow);
+              subs += (variable -> arrow);
+            }
+            
+          //case of a var mapping to a var, will need to do another pass
+          case (vara:VarType, varb:VarType) =>
+            if(subs.contains(vara) && !subs(vara).equals(varb)){
+              return None;
+            } else{
+              //TODO check for circuilar dependecy (var type = itself)
+              still_propigations |= true;
+              constraints = replaceAllVarWithType(constraints, vara, varb);
+              subs += (vara -> varb);
+            }
+            
+          //case of an arrow to arrow, generate two additional constraints
+          case (arrowa: ArrowType, arrowb:ArrowType) =>
+            constraints += new Tuple2(arrowa.src,arrowb.src);
+            constraints += new Tuple2(arrowa.dst,arrowb.dst);
+        }
+      }
     }
-    
+
     return Some(subs.toMap);
   }
-  
+
+  //find and replace all matching variables (whether prefix, postfix, or in arrow type in postfix) with the type t
+  def replaceAllVarWithType(constraints: scala.collection.mutable.ListBuffer[Tuple2[Type,Type]], variable:VarType, t:Type) : scala.collection.mutable.ListBuffer[Tuple2[Type,Type]] = {
+    var new_constraints = scala.collection.mutable.ListBuffer[Tuple2[Type,Type]]();
+    
+    for(i <- 0 until constraints.length){
+      if(constraints(i)._1.equals(variable)){
+        new_constraints += new Tuple2(t, constraints(i)._2);
+      }
+      else if(constraints(i)._2.equals(variable)){
+        new_constraints += new Tuple2(constraints(i)._1, t);
+      }
+      //TODO arrow types
+    }
+    
+    return new_constraints;
+  }
 }
