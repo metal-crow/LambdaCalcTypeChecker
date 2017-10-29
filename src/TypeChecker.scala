cbf87b0657a522ab563ee7c2bda34c02c98c6d55
metal-crow <frankcrisanti@aol.com>
2017-10-29 15:52:01 -0400

    About finished with typechecker

--- src/TypeChecker.scala
+++ src/TypeChecker.scala
@@ -44,15 +44,43 @@ object TypeChecker {
         }
         return None;
       }
-      case le:Let => return handleLet(le.st, typeCheck(le.body, environment));
+      //let is just a straightforward substitution, so handle it and get the resulting expression's type
+      case le:Let => {
+        //convert into a map of id => expressions instead of weird resursive method
+        val letMap = generateLetMap(le.st);
+        //use that map to substitute
+        return typeCheck(handleLet(letMap, le.body), environment);
+      }
     }
     return None;
   }
   
-  def handleLet(state: Stmt, body: Option[Type]) : Option[Type] = {
-    state match {
-      case _: Empty => return None;
-      case as: Assign => 
+  def generateLetMap(st: Stmt) : Map[String, Exp] = {
+    st match{
+      case _: Empty => return Map();
+      case a: Assign => return Map((a.lhs, a.rhs));
+      case s: Seq => return generateLetMap(s.left) ++ generateLetMap(s.right);//right will overwrite left if needed
+    }
+  }
+  
+  def handleLet(map: Map[String, Exp], ex: Exp) : Exp = {
+    ex match{
+      case v: Var => {
+        if(map.keySet.contains(v.id)){
+          return map(v.id);
+        }else{
+          return v;
+        }
+      }
+      case n: Num => return n;
+      case b: Bool => return b;
+      case la: Lambda => return new Lambda(la.binder, handleLet(map, la.body));
+      case app: Application => return new Application(handleLet(map, app.left), handleLet(map, app.right));
+      case cond: Conditional => return new Conditional(handleLet(map, cond.cond), handleLet(map, cond.conseq), handleLet(map, cond.alter));
+      case le: Let => {
+        val letMap = map ++ generateLetMap(le.st);//right will overwrite left if needed
+        return handleLet(letMap, le.body);
+      }
     }
   }
 }
